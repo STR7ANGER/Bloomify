d23e74063c98aeca34603b76d793c29e12beeab1
Arkadip <arkadip671@gmail.com>
2025-03-17 12:21:38 +0530

    updated logo and file structuring

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Nav = () => {
   return (
     <div>
-      <p>Hello there</p>
+      
     </div>
   )
 }
 
-export default Home
+export default Nav
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import bloomifyBlack from '../assets/logos/bloomify-black.png';
+
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hello there</p>
+      <img className='h-[220px] w-[220px]' src={bloomifyBlack} alt="logo" />
+    </div>
+  )
+}
+
+export default Home
